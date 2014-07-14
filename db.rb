3e20627e636068304a3059e4a86bd11a93ea8907
Sam Couch <samcouch91@gmail.com>
2014-07-14 11:28:05 -0700

    Fix

--- db.rb
+++ db.rb
@@ -7,6 +7,6 @@ require "pathological"
 
 require "config/environment"
 
-LOCAL_DB = { :host => DB_HOST, :user => DB_USER, :password => DB_PASSWORD, :database => DB_DATABASE }
+LOCAL_DB = { :adapter=> 'postgres', :host => DB_HOST, :user => DB_USER, :password => DB_PASSWORD, :database => DB_DATABASE }
 
-DB = Sequel.postgres(ENV['DATABASE_URL'] || LOCAL_DB)
+DB = Sequel.connect(ENV['DATABASE_URL'] || LOCAL_DB)
