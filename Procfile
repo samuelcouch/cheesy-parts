ecc91e48f18fe45c76e5ae16421426c5ff3ae12e
Sam Couch <samcouch91@gmail.com>
2014-07-14 10:38:39 -0700

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec ruby parts_server_control.rb -p $PORT
--- models/task.rb
+++ models/task.rb
@@ -1,25 +1,22 @@
 class Task < Sequel::Model
   many_to_one :project
   STATUS_MAP = { "assigned" => "Assigned to ",
-                              "planning" => "Creating a plan of action.",
-                              "processing" => "In progress.",
-                              "completed" => "Completed." }
+                           "planning" => "Creating a plan.",
+                           "processing" => "In progress.",
+                           "completed" => "Completed." }
   
   PRIORITY_MAP = { 0 => "High", 1 => "Normal", 2 => "Low" }
 
-  def self.generate_number_and_create(project, type, parent_part)
-    parent_part_id = parent_part.nil? ? 0 : parent_part.id
-    parent_part_number = parent_part.nil? ? 0 : parent_part.part_number
+  def self.generate_number_and_create(project, type)
     if type == "part"
-      part_number = Part.filter(:project_id => project.id, :parent_part_id => parent_part_id, :type => "part")
-                        .max(:part_number) || parent_part_number
+      task_number = Task.filter(:project_id => project.id, :type => "task")
+                        .max(task_number) || parent_part_number
       part_number += 1
     else
-      part_number = Part.filter(:project_id => project.id, :type => "assembly").max(:part_number)  || -100
+      part_number = Task.filter(:project_id => project.id, :type => "assembly").max(:part_number)  || -100
       part_number += 100
     end
-    new(:part_number => part_number, :project_id => project.id, :type => type,
-        :parent_part_id => parent_part.nil? ? 0 : parent_part.id)
+    new(:part_number => part_number, :project_id => project.id, :type => type)
   end
 
   def full_part_number
